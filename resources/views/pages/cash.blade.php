baf44adb7ce8873ba22bf45f2bf4c2b8e2e7c005
zhoriefzainulmuttaqin <zhoriefzainulmuttaqin@gmail.com>
2023-04-26 00:05:45 +0700

    cash

--- resources/views/pages/cash.blade.php
+++ resources/views/pages/cash.blade.php
@@ -6,23 +6,54 @@
                 @csrf
                 <h4 class="mb-3">Cash Payment</h4>
                 <div class="d-flex p-0 mt-4 justify-content-between">
-                    <div class="dropdown ms-4">
-                        <button class="btn bg-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Select Santri
-                        </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton"
-                            style="max-height: 200px; overflow-y: auto;">
-                            <li><a class="dropdown-item" href="#">
-                                    uu
-                            </li>
-                        </ul>
+                    <div class="dropdown">
+                        <table>
+                            <th class="col-5">Date</th>
+                            <th class=""> <span class="" id="selectedSantri">
+                                    <input class="form-control" type="date" value="0" id="example-number-input"
+                                        name="qty" min="0">
+                                </span></th>
+                        </table>
+                        <table class="mt-3">
+                            <th class="col-5">No</th>
+                            <th class=""> <span class="" id="selectedSantri">
+                                    <input class="form-control" type="number" value="0" id="example-number-input"
+                                        name="qty" min="0">
+                                </span></th>
+                        </table>
+                        <table class="mt-3">
+                            <th class="col-5">Name</th>
+                            <th class=""> <span class="" id="selectedSantri">
+                                    <input class="form-control" type="number" value="0" id="example-number-input"
+                                        name="qty" min="0">
+                                </span></th>
+                        </table>
+                        <table class="mt-3">
+                            <th class="col-5">Total Cash</th>
+                            <th class=""> <span class="" id="selectedSantri">
+                                    <input class="form-control" type="number" value="0" id="example-number-input"
+                                        name="qty" min="0">
+                                </span></th>
+                        </table>
+                        <table class="mt-3">
+                            <th class="col-5">Grand Total</th>
+                            <th class=""> <span class="ms-3" id="selectedSantri">
+                                    10000
+                                </span></th>
+                        </table>
+                        <table class="mt-3">
+                            <th class="col-5">Change</th>
+                            <th class=""> <span class="ms-5" id="selectedSantri">
+                                    10000
+                                </span></th>
+                        </table>
                     </div>
-                    <div>
-                        <span class="ms-4 me-4" id="selectedSantri">
-                            Santri Selected
+                    {{-- <div class="me-10">
+                        <span class="" id="selectedSantri">
+                            <input class="form-control" type="number" value="0" id="example-number-input"
+                                name="qty" min="0">
                         </span>
-                    </div>
+                    </div> --}}
                 </div>
             </form>
         </div>
